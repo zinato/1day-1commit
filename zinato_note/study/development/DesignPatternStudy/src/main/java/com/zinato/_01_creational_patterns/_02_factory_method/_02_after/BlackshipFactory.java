7f3ec9d41b94fe55b1f6836122f76979c7d18705
hyunil.jang <hyunil@braincolla.com>
2022-01-03 01:49:11 +0900

    study: FactoryMethod After 코드 작성

---
+++ zinato_note/study/development/DesignPatternStudy/src/main/java/com/zinato/_01_creational_patterns/_02_factory_method/_02_after/BlackshipFactory.java
@@ -0,0 +1,8 @@
+package com.zinato._01_creational_patterns._02_factory_method._02_after;
+
+public class BlackshipFactory implements ShipFactory {
+  @Override
+  public Ship createShip() {
+    return new Blackship();
+  }
+}
