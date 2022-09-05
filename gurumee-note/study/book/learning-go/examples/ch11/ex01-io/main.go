c23a4cd39730346961f466a0212bf679666bf894
gurumee92 <gurumee92@gmail.com>
2022-09-06 08:42:33 +0900

    code: Create ch11 1. 1절 io 예제 추가 2. 2절 time 예제 추가

---
+++ gurumee-note/study/book/learning-go/examples/ch11/ex01-io/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
+
+func countLetters(r io.Reader) (map[string]int, error) {
+	buf := make([]byte, 2048)
+	out := map[string]int{}
+
+	for {
+		n, err := r.Read(buf)
+		for _, b := range buf[:n] {
+			if (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				out[string(b)]++
+			}
+		}
+
+		if err == io.EOF {
+			return out, nil
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+
+func main() {
+	s := "The quick brown fox jumped over the lazy dog"
+	sr := strings.NewReader(s)
+
+	counts, err := countLetters(sr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(counts)
+}
