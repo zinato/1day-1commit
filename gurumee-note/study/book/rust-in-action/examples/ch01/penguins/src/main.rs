0812c8b6d69678e78ba0eb4f3865e37d01359e04
장현우 <gurumee92@gmail.com>
2022-09-30 21:33:11 +0900

    code: Create book/rust-in-action/examples/ch01/penguins

---
+++ gurumee-note/study/book/rust-in-action/examples/ch01/penguins/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let penguin_data = "\
+    common name, length (cm)
+    Little,33
+    Yello,65
+    Fiordland,60
+    Invalid,data
+    ";
+
+    let records = penguin_data.lines();
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
+        if cfg!(debug_asserttions) {
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() {
+            println!("{}, {}cm", name, length);
+        }
+    }
+}
